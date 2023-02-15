cffba27b5f25cdf2a727cfbd12f68ea10a392abd
Melissa Hernandez <102121286+mmhernandez@users.noreply.github.com>
2023-02-15 12:20:49 -0800

    Update intermediate_sums.py

--- intermediate_sums.py
+++ intermediate_sums.py
@@ -6,42 +6,34 @@ def intermediate_sums(list):
     chunk = 10
 
     #loop through the list in chunks of 10
-    # (the incrementer is 11 b/c the sum is inserted into the list after each group of 10)
-    for i in range(0, len(list), 11):
-        print(f"------i loop {i}------\nlist[i] = {list[i]}")
-        print(f"len(list)-i = {len(list)-i}")
+    # the incrementer is 11 b/c the sum is inserted into the list after each group of 10
+    # the high-bound of the range needs to be increased to include the additional sum values being inserted in
+    for i in range(0, len(list) + int((len(list)/10)) - 1, 11):
         #check list length at each chunk to determine sum workflow
         if len(list)-i > 10:
-            for j in range(i, chunk):   #loop through the first chunk of 10
-                print(f"------j loop {j}------")
-                print(f"chunk = {chunk}")
-                sum += list[j]      #sum values for the chunk of 10
-                print(f"sum = {sum}")
-            print(list)
+            for j in range(i, chunk):   #loop through the chunk of 10
+                sum += list[j]          #sum values for the chunk of 10
 
-            #loop through remaining list indecies to make room for sum    
+            #append the last list value to the end of the list, so everything can move forward one place in the list to make room for the sum of the chunk of 10 
             list.append(list[len(list)-1])
-            print(list)
-            print(f"chunk = {chunk}")
+
+            #loop through remaining list indecies to make room for sum 
             for k in range(len(list)-1, chunk, -1):
-                print(f"------k loop {k}------")
-                print(list)
                 list[k-1] = list[k-2]
-                print(list)
-            print(f"sum = {sum}")
-            print(f"j+1 = {j+1}")
-            print(f"list[j+1] = {list[j+1]}")
-            list[chunk] = sum         #insert the sum into the list
-            print(list)
+
+            list[chunk] = sum   #insert the sum into the list at the end of the chunk
         else:
             for l in range(i,len(list)):
-                print(f"l = {l}")
                 sum += list[l]
             list.append(sum)
-        sum = 0     #reset the sum for the next chunk
-        chunk *= 2
-
+        
+        sum = 0             #reset the sum for the next chunk
+        chunk = chunk + 11  #increase the chunk variable for the next group of 10 (increases by 11 to account for additional index inserted with the chunk's sum)
+    
     return list
 
-
-print(intermediate_sums([1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5]))
+print(intermediate_sums([1,2,3,4,5,6]))
+print(intermediate_sums([1,2,3,4,5,6,7,8,9,10]))
+print(intermediate_sums([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]))
+print(intermediate_sums([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]))
+print(intermediate_sums([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]))
